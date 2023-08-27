cc88e78046e7e49511615e889614a9e9002f212a
hoanpm <hoan922003@gmail.com>
2023-08-27 13:06:31 +0700

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Vũ Hoàn on 11/02/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
